e6b92b979d91282ee03a54f3412468eff1fb93ed
RamithaHeshan33 <st20286184@outlook.cardiffmet.ac.uk>
2024-05-01 19:04:37 +0530

    currency app

--- server/src/index.js
+++ server/src/index.js
@@ -23,6 +23,32 @@ app.get("/getAllCurrencies", async (req,res) => {
     }
 })
 
+//get the target amount
+app.get("/convert", async (req, res) => { // <-- Corrected order of parameters (req, res)
+    const {date, sourceCurrency, targetCurrency, amountInSourceCurrency} = req.query;
+
+    try {
+        const dataURL = `https://openexchangerates.org/api/historical/${date}.json?app_id=d066ba15a96345a68af34df1d3ad7740`
+        
+        const dataResponse = await axios.get(dataURL);
+        const rates = dataResponse.data.rates;
+        
+        //rates
+        const sourceRate = rates[sourceCurrency];
+        const targetRate = rates[targetCurrency];
+
+        //final target value 
+        const targetAmo = (targetRate / sourceRate) * amountInSourceCurrency;
+
+        return res.json(targetAmo.toFixed(2));
+    }
+    catch(e) {
+        console.log(e);
+        return res.status(500).json({ error: 'Internal Server Error' }); // Return an error response
+    }
+});
+
+
 //listen to a part
 app.listen(5000, () => {
     console.log("Server Started");
